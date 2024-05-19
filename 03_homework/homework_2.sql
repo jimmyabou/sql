b17e9773b9cb39c4f97d4cb263f978479d4a0a3d
jimabou <128553101+jimmyabou@users.noreply.github.com>
2024-05-19 08:40:09 -0400

    Update homework_2.sql

--- 03_homework/homework_2.sql
+++ 03_homework/homework_2.sql
@@ -53,7 +53,7 @@ CASE
 	ELSE 'bulk'
 	END AS prod_qty_type_condensed,
 CASE
-	WHEN LOWER(product_name) LIKE "%pepper%" THEN 1
+	WHEN LOWER(product_name) LIKE '%pepper%' THEN 1
 	ELSE 0
 	END AS pepper_flag
 FROM product;
